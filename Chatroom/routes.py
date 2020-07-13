749e1a4b296121273407f543721fef0ba00521b2
Tu Vu <vuanhtu0305@gmail.com>
2020-07-13 11:04:48 -0500

    Add create new channel function

--- Chatroom/routes.py
+++ Chatroom/routes.py
@@ -1,5 +1,5 @@
 """ Routes for page content """
-from flask import Blueprint, url_for, render_template, request, redirect, flash
+from flask import Blueprint, url_for, render_template, request, redirect, flash, jsonify
 from flask_login import current_user, login_required
 from .forms import *
 from .models import *
@@ -15,9 +15,21 @@ main_bp = Blueprint(
 
 @main_bp.route("/dashboard", methods=["GET", "POST"])
 def dashboard(): 
-
-    # # Check if user just sent a message
-    # if request.method == "POST":
-    #     request.form.get("message")
-
     return render_template("dashboard.html")
+
+''' CREATE NEW CHANNEL '''
+@main_bp.route("/create", methods=["POST"])
+def create():
+    channel_name = request.form.get("channel_name")
+
+    # Check if the channel already exists in database
+    existing_channel = Channel.query.filter_by(name=channel_name).first()
+
+    # Only add new channel
+    if existing_channel is None:
+        new_channel = Channel(name=channel_name)
+        db.session.add(new_channel)
+        db.session.commit()
+        return jsonify({"success": True})
+    else:
+        return jsonify({"success": False})
