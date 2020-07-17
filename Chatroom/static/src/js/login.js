59173074bf0280259a777a990b45b896d91c8c52
Duong Nguyen <duongnguyen@duongmacbook.local>
2020-07-17 13:02:44 -0500

    "New login style and signup style"

---
+++ Chatroom/static/src/js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   			$("#button").mouseenter(function(){ 
+  				$("#button").attr("class", "btn1 success");
+			});
+			$("#button").mouseleave(function(){ 
+  				$("#button").attr("class", "btn1 success1");
+			});
+			 
+});
